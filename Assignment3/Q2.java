35f99061d85fc24180eda3120b4d49270a0e7147
Shazad <md.shazad@tothenew.com>
2022-09-30 16:40:30 +0530

    assignment3

--- Assignment3/Q2.java
+++ Assignment3/Q2.java
@@ -1,10 +1,66 @@
 package Assignment3;
 
+import java.util.*;
+import java.util.Collections;
+import java.util.Comparator;
+
+class Employee implements Comparable<Employee>{
+	double age;
+	double salary;
+	String name;
+	
+	
+	Employee(Double age, Double salary, String name){
+		this.age = age;
+		this.salary = salary;
+		this.name = name;
+	}
+	public Double getSalary() {
+		return salary;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	@Override
+	public int compareTo(Employee emp) {
+		return this.name.compareTo(emp.getName());
+	}
+	
+	@Override
+	public String toString() {
+		return this.name + ", " + this.salary;
+	}
+	
+}
 public class Q2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Employee emp1 = new Employee(33.0,4555.33,"Joice Parker");
+		Employee emp2 = new Employee(34.0,38388.0,"Pan Peter");
+		Employee emp3 = new Employee(35.0,89999.0,"Hrithik roshan");
+		
+		List<Employee> employees = new ArrayList<Employee>();
+		
+		employees.add(emp1);
+		employees.add(emp2);
+		employees.add(emp3);
+		
+		System.out.println("By default sort using name overriding compareTo: ");
+		System.out.println(employees);
+		
+		Collections.sort(employees,new Comparator<Employee>(){
+			@Override
+			public int compare(Employee e1,Employee e2) {
+				return((int) (e1.getSalary()-e2.getSalary()));
+			}
+		});
+		
+		System.out.println("Sorted by salary: ");
+		System.out.println(employees);
+		//sort based on salary
+		
 	}
 
 }
--- Assignment3/Q4.java
+++ Assignment3/Q4.java
@@ -2,33 +2,12 @@ package Assignment3;
 
 import java.util.*;
 
-public class Q4 {
-
-	public static void main(String[] args) {
-		Employee emp1 = new Employee("Ravi",26,"Sales");
-		Employee emp2 = new Employee("jagdamba",24,"SE");
-		Employee emp3 = new Employee("Shazad",32,"SSE");
-		
-		Map<Employee,Integer> map = new HashMap<>();
-		
-		map.put(emp3, 6677);
-		map.put(emp2,  6788);
-		map.put(emp1, 6888);
-		
-		for(Employee key: map.keySet()) {
-			System.out.println(key + "Salary: " + map.get(key));
-		}
-
-	}
-
-}
-
-class Employee{
+class Employees{
 	private String name;
 	private int age;
 	private String des;
 	
-	public Employee(String name, int age,String des) {
+	public Employees(String name, int age,String des) {
 		this.name=name;
 		this.age=age;
 		this.des=des;
@@ -38,3 +17,24 @@ class Employee{
 		return "Name of Employee" + name + "Age:" + age + " designation:"+des;
 	}
 }
+public class Q4 {
+
+	public static void main(String[] args) {
+		Employees emp1 = new Employees("Ravi",26,"Sales");
+		Employees emp2 = new Employees("jagdamba",24,"SE");
+		Employees emp3 = new Employees("Shazad",32,"SSE");
+		
+		Map<Employees,Integer> map = new HashMap<>();
+		
+		map.put(emp3, 6677);
+		map.put(emp2,  6788);
+		map.put(emp1, 6888);
+		
+		for(Employees key: map.keySet()) {
+			System.out.println(key + " Salary: " + map.get(key));
+		}
+
+	}
+
+}
+
